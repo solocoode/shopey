8f7923c51c0b19ea87250ee6c12dccbb9e628016
solocoode <solonjogo@gmail.com>
2023-03-10 02:32:41 +0300

    updated commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
 
+    path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
